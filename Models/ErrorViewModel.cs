7058c7b85de43f0b5824025066838678108bae9c
Roberto <Roberto@ROBERTOOROZCO>
2021-09-14 10:45:00 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace blazorwasm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
